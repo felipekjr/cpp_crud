ede49da7c6e48241db8a3173fff0508f86d944f6
Felipe Rodrigues do Nascimento <felipekjr@A204-08L.imd.local>
2019-05-02 11:09:33 -0300

    Checar se nome do funcionário está vazio

--- telas/funcionario-crud/funcionario-crud.cpp
+++ telas/funcionario-crud/funcionario-crud.cpp
@@ -12,30 +12,39 @@
 * @return Funcionario criado
 */
 
-Funcionario criarFuncionario() {
+Funcionario criarFuncionario() {    
     string nome, inputDate, salario;     
     Data dataAdmissao;  
     Funcionario funcionario;
-    cout << CYAN<<"--- FUNCIONÁRIO ---"<<RESET<< endl;
-    cout << "- nome: ";
-    cin >> nome;
-    cout << "- salário: ";
-    cin >> salario;
-    if(isDouble(salario)){
-        cout << "- data de admissão (dd/mm/yyyy): ";
-        cin >> inputDate;   
-        dataAdmissao = stringToData(inputDate);
-        if(checarDataValida(dataAdmissao)){           
-            funcionario = Funcionario(nome, stod(salario), dataAdmissao);   
-            return funcionario;
-        } else {
-            cout << RED << "Data inválida!" << RESET << endl;
-            throw std::exception();    
+    bool invalidInput;
+    do {
+        cout << CYAN<<"--- FUNCIONÁRIO ---"<<RESET<< endl;
+        cout << "- nome: ";
+        cin.ignore();
+        getline(cin, nome);
+        cout << "- salário: ";
+        getline(cin, salario);    
+        if(isDouble(salario)){
+            cout << "- data de admissão (dd/mm/yyyy): ";
+            cin >> inputDate;   
+            dataAdmissao = stringToData(inputDate);
+            if(checarDataValida(dataAdmissao)){
+            } else {
+                cout << RED << "Data inválida!" << RESET << endl;
+                throw std::exception();    
+            }  
+        } else {  
+            cout << RED << "Salário inválido!" << RESET << endl;
+            throw std::exception(); 
         }  
-    } else {  
-        cout << RED << "Salário inválido!" << RESET << endl;
-        throw std::exception(); 
-    }    
+        if(isEmpty(nome)){
+            invalidInput = true;
+            cout << YELLOW << "Nenhum campo pode ser vazio!" << RESET << endl;
+        }
+    }while(invalidInput);
+    funcionario = Funcionario(nome, stod(salario), dataAdmissao);   
+    return funcionario;
+     
     
 }
 
